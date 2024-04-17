482c4e43dd43cf6a078f6601afcae91bf5b763d0
Whitney_vader <whitney@gmail.com>
2024-04-17 16:17:24 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+  name: 'John Doe',
+  streetAddress: '123 Main St'
+};
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => ({
+  ...employee,
+  [key]: value
+});
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+  employee[key] = value;
+  return employee;
+};
+
+const deleteFromEmployeeByKey = (employee, key) => ({
+  ...Object.keys(employee).filter(k => k !== key).reduce((acc, curr) => {
+    acc[curr] = employee[curr];
+    return acc;
+  }, {})
+});
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+  delete employee[key];
+  return employee;
+};
